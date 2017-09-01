2db3ac18d76b0f540336a7c8e3b08bb8e369e1f2
Cedric Haase <cedric@sineband.de>
2017-09-02 01:29:00 +0200

    Migrate layout to css grid and add electron

---
+++ main.js
@@ -0,0 +1,45 @@
+// ./main.js
+const {app, BrowserWindow} = require('electron');
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({
+    width: 1000, height: 600,
+    transparent: true,
+    frame: false
+    // titlebarStyle: 'hidden'
+  });
+
+  // Specify entry point
+  win.loadURL('http://monolith:4200');
+
+  // Show dev tools
+  // Remove this line before distributing
+  // win.webContents.openDevTools();
+
+  win.once('ready-to-show', () => {
+    win.show()
+  });
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
